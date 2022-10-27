07788cb557d2fc5fc9ca073d69b940bebddd159e
Jovi <kwong.jovi@gmail.com>
2022-10-27 23:44:37 +0100

    update(test script): add 2 AgedBrie objects to items array

--- test/test_scripts/script.js
+++ test/test_scripts/script.js
@@ -1,9 +1,12 @@
 const Shop = require("../../src/shop");
 const RegularItem = require("../../src/items/regular_item");
+const AgedBrie = require("../../src/items/aged_brie");
 
 const items = [
   new RegularItem("Regular Item One", 100, 50),
-  new RegularItem("Regular Item One", 0, 50),
+  new RegularItem("Regular Item Two", 0, 50),
+  new AgedBrie("Aged Brie One", 10, 49),
+  new AgedBrie("Aged Brie Two", 0, 10),
 ];
 
 const days = Number(process.argv[3]) || 3;
