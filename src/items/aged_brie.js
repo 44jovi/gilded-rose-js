e700f5d3fa9678622515b901621abb89d0583417
Jovi <kwong.jovi@gmail.com>
2022-10-27 23:43:49 +0100

    feat(AgedBrie): test-drive upupdateItemSellIn() and updateItemupdateItemQuality().

---
+++ src/items/aged_brie.js
@@ -0,0 +1,30 @@
+const Item = require("./item");
+
+class AgedBrie extends Item {
+  updateItemSellIn() {
+    // If sell by date is already zero
+    if (this.sellIn <= 0) {
+      this.sellIn = 0;
+      // Otherwise continue with decrement
+    } else {
+      this.sellIn -= 1;
+    }
+  }
+
+  updateItemQuality() {
+    // Aged Brie item quality increases in value over time
+    // Need inn owners to clarify the following is the desired behaviour!
+
+    // If quality value is between 0 and 50 (inclusive)
+    if (this.quality >= 0 && this.quality < 50) {
+      // If before sell by date
+      if (this.sellIn > 0) {
+        // Quality value increases
+        this.quality += 1;
+      }
+    }
+    // Otherwise quality value remains unchanged
+  }
+}
+
+module.exports = AgedBrie;
--- test/unit/item/aged_brie.test.js
+++ test/unit/item/aged_brie.test.js
@@ -1,7 +1,7 @@
 const AgedBrie = require("../../../src/items/aged_brie");
 
 describe("`AgedBrie` - child class of `Item`", () => {
-  xit("updateItemSellIn() updates item's sellIn value", () => {
+  it("updateItemSellIn() updates sellIn value", () => {
     const agedBrie = new AgedBrie("Aged Brie", 10, 50);
 
     expect(agedBrie.name).toEqual("Aged Brie");
@@ -11,31 +11,51 @@ describe("`AgedBrie` - child class of `Item`", () => {
 
     expect(agedBrie.sellIn).toEqual(9);
   });
+
+  it("updateItemSellIn() cannot decrease `sellIn` value lower than zero", () => {
+    const agedBrie = new AgedBrie("Aged Brie", 0, 50);
+
+    expect(agedBrie.name).toEqual("Aged Brie");
+    expect(agedBrie.sellIn).toEqual(0);
+
+    agedBrie.updateItemSellIn();
+
+    expect(agedBrie.sellIn).toEqual(0);
+  });
+
+  it("updateItemQuality() increases `quality` value (max value = 50)", () => {
+    const agedBrie = new AgedBrie("Aged Brie", 10, 49);
+
+    expect(agedBrie.name).toEqual("Aged Brie");
+    expect(agedBrie.sellIn).toEqual(10);
+    expect(agedBrie.quality).toEqual(49);
+
+    agedBrie.updateItemQuality();
+    agedBrie.updateItemSellIn();
+
+    expect(agedBrie.sellIn).toEqual(9);
+    expect(agedBrie.quality).toEqual(50);
+
+    agedBrie.updateItemQuality();
+    agedBrie.updateItemSellIn();
+
+    expect(agedBrie.sellIn).toEqual(8);
+    // Quality does not go beyond 50
+    expect(agedBrie.quality).toEqual(50);
+  });
+
+  it("updateItemQuality() does not increase `quality` value after sell by date", () => {
+    const agedBrie = new AgedBrie("Aged Brie", 0, 49);
+
+    expect(agedBrie.name).toEqual("Aged Brie");
+    expect(agedBrie.sellIn).toEqual(0);
+    expect(agedBrie.quality).toEqual(49);
+
+    agedBrie.updateItemQuality();
+    agedBrie.updateItemSellIn();
+
+    expect(agedBrie.sellIn).toEqual(0);
+    // Quality remains unchanged
+    expect(agedBrie.quality).toEqual(49);
+  });
 });
-
-// it("updateItemSellIn() cannot decrease an item's sellIn value lower than zero", () => {
-//   const agedBrie = new AgedBrie("Aged Brie", 0, 50);
-
-//   expect(agedBrie.name).toEqual("Aged Brie");
-//   expect(agedBrie.sellIn).toEqual(0);
-
-//   agedBrie.updateItemSellIn();
-
-//   expect(agedBrie.sellIn).toEqual(0);
-// });
-
-// it("updateItemQuality() increases 'Aged Brie' item's quality (max value = 50)", () => {
-//   const agedBrie = new AgedBrie("Aged Brie", 10, 49);
-
-//   expect(agedBrie.name).toEqual("Aged Brie");
-//   expect(agedBrie.sellIn).toEqual(10);
-//   expect(agedBrie.quality).toEqual(1);
-
-//   agedBrie.updateItemSellIn();
-//   agedBrie.updateItemQuality();
-
-//   expect(agedBrie.sellIn).toEqual(9);
-//   expect(agedBrie.quality).toEqual(2);
-// });
-
-// if sellIn = 0, quality stops changing
