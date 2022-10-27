8e612b59f65831ebe0df99efb5ab141200ea91e6
Jovi <kwong.jovi@gmail.com>
2022-10-27 13:18:46 +0100

    test(Shop): test suite initial setup

---
+++ test/shop.test.js
@@ -0,0 +1,13 @@
+const Shop = require("../src/shop");
+
+// Future: use beforeEach() to reduce code unnecessary code repetition
+
+describe("Item", () => {
+  describe("updateQuality()", () => {
+    it("", () => {
+      shop = new Shop([]);
+
+      expect(shop.updateItemQuality()).toEqual([]);
+    });
+  });
+});
