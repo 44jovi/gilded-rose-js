0aa2deb3ccd4ceb9725c46b75706d83dd59726fd
Jovi <kwong.jovi@gmail.com>
2022-10-28 14:48:50 +0100

    feat(BackstagePass): test-drive. Pass unit tests

--- test/unit/item/backstage_pass.test.js
+++ test/unit/item/backstage_pass.test.js
@@ -39,7 +39,7 @@ describe("`BackstagePass` - child class of `RegularItem`", () => {
     expect(backstagePass.quality).toEqual(29);
   });
 
-  it("updateItemQuality() increases `quality` more than by 1 based on `sellIn` value. Max quality = 50", () => {
+  it("updateItemQuality() increases `quality` by more than 1 based on `sellIn` value. Max quality = 50", () => {
     // Scenario 1
     // While item `sellIn` value <= 10 and > 5
     const backstagePass1 = new BackstagePass("Backstage Pass 1", 10, 48);
@@ -79,24 +79,25 @@ describe("`BackstagePass` - child class of `RegularItem`", () => {
   });
 
   it("updateItemQuality() changes `quality` to 0 when `sellIn` value <= 0", () => {
-    const backstagePass1 = new BackstagePass("Backstage Pass 1", 1, 50);
+    const backstagePass = new BackstagePass("Backstage Pass 1", 1, 50);
 
-    expect(backstagePass1.name).toEqual("Backstage Pass 1");
-    expect(backstagePass1.sellIn).toEqual(1);
-    expect(backstagePass1.quality).toEqual(50);
+    expect(backstagePass.name).toEqual("Backstage Pass 1");
+    expect(backstagePass.sellIn).toEqual(1);
+    expect(backstagePass.quality).toEqual(50);
 
     // Update 1
-    backstagePass1.updateItemQuality();
-    backstagePass1.updateItemSellIn();
+    backstagePass.updateItemQuality();
+    backstagePass.updateItemSellIn();
 
-    expect(backstagePass1.sellIn).toEqual(0);
+    expect(backstagePass.sellIn).toEqual(0);
+    expect(backstagePass.quality).toEqual(50);
 
     // Update 2
-    backstagePass1.updateItemQuality();
-    backstagePass1.updateItemSellIn();
+    backstagePass.updateItemQuality();
+    backstagePass.updateItemSellIn();
 
+    expect(backstagePass.sellIn).toEqual(0);
     // Quality changes to 0
-    expect(backstagePass1.sellIn).toEqual(0);
-    expect(backstagePass1.quality).toEqual(0);
+    expect(backstagePass.quality).toEqual(0);
   });
 });
