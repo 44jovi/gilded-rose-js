8e51eb5b27e73fd3fc1488d6a06663bac037d9ca
Jovi <kwong.jovi@gmail.com>
2022-10-28 11:22:49 +0100

    feat(ConjuredItem): test-drive. Pass unit tests

--- src/items/conjured_item.js
+++ src/items/conjured_item.js
@@ -1,28 +1,17 @@
-const Item = require("./item");
-
-// Future: Consider extending RegularItem instead
-
-class ConjuredItem extends Item {
-  updateItemSellIn() {
-    // If sell by date is already zero
-    if (this.sellIn <= 0) {
-      this.sellIn = 0;
-      // Otherwise continue with decrement
-    } else {
-      this.sellIn -= 1;
-    }
-  }
-
+const RegularItem = require("./regular_item");
+class ConjuredItem extends RegularItem {
   updateItemQuality() {
+    // Conjured items value decreases twice as fast as regular items
+
     // If quality is already zero
     if (this.quality <= 0) {
       this.quality = 0;
       // Before sell by date
     } else if (this.sellIn > 0) {
-      this.quality -= 1;
+      this.quality -= 2;
       // After sell by date
     } else {
-      this.quality -= 2;
+      this.quality -= 4;
     }
   }
 }
