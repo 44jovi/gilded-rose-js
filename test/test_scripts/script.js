0291546df927da0649b250db76df080b9e7fd55a
Jovi <kwong.jovi@gmail.com>
2022-10-28 12:05:55 +0100

    test(test script): add Sulfuras to script.js

--- test/test_scripts/script.js
+++ test/test_scripts/script.js
@@ -2,14 +2,17 @@ const Shop = require("../../src/shop");
 const RegularItem = require("../../src/items/regular_item");
 const AgedBrie = require("../../src/items/aged_brie");
 const ConjuredItem = require("../../src/items/conjured_item");
+const Sulfuras = require("../../src/items/sulfuras");
 
 const items = [
   new RegularItem("Regular Item One", 100, 50),
   new RegularItem("Regular Item Two", 0, 50),
   new AgedBrie("Aged Brie One", 10, 49),
   new AgedBrie("Aged Brie Two", 0, 10),
-  new ConjuredItem("Conjured Mana Cake", 100, 50),
-  new ConjuredItem("Conjured Mana Cake", 0, 50),
+  new ConjuredItem("Conjured Mana Cake One", 100, 50),
+  new ConjuredItem("Conjured Mana Cake Two", 0, 50),
+  new Sulfuras("Sulfuras, Hand of Ragnaros One", 0, 50),
+  new Sulfuras("Sulfuras, Hand of Ragnaros Two", 100, 100),
 ];
 
 const days = Number(process.argv[3]) || 3;
