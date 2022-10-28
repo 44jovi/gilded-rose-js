4282eb623ea4eec0e67bc8650e4bd8d724ae486c
Jovi <kwong.jovi@gmail.com>
2022-10-28 11:23:38 +0100

    test(test scripts): add ConjuredItem objects to array in script.js

--- test/test_scripts/script.js
+++ test/test_scripts/script.js
@@ -1,12 +1,15 @@
 const Shop = require("../../src/shop");
 const RegularItem = require("../../src/items/regular_item");
 const AgedBrie = require("../../src/items/aged_brie");
+const ConjuredItem = require("../../src/items/conjured_item");
 
 const items = [
   new RegularItem("Regular Item One", 100, 50),
   new RegularItem("Regular Item Two", 0, 50),
   new AgedBrie("Aged Brie One", 10, 49),
   new AgedBrie("Aged Brie Two", 0, 10),
+  new ConjuredItem("Conjured Mana Cake", 100, 50),
+  new ConjuredItem("Conjured Mana Cake", 0, 50),
 ];
 
 const days = Number(process.argv[3]) || 3;
